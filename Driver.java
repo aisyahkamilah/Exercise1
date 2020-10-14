713a1783a0aefb1bd6b8d67646da81e4e2cb4cd7
aisyahkamilah <72842595+aisyahkamilah@users.noreply.github.com>
2020-10-14 14:51:16 +0800

    Add files via upload

--- Driver.java
+++ Driver.java
@@ -4,17 +4,32 @@ public class Driver {
 	public static void main(String [] arg) {
 		//Fish actually is a data type
 		//Fish nemo; == int x;
-		Fish nemo = new Fish();  //nemo is object, Fish is class
+		//nemo is object, Fish is class
 
+		Fish nemo = new Fish();  
 		nemo.Swim(); 
-		nemo.color = "red"; //tukar value
+		//nemo.color = "red"; //tukar value
+		nemo.SetColor("red"); //change here
 		nemo.Swim();
 
-		Fish dory = new Fish(); //another object
+		//nemo.Eat(); //superclass call subclass method - not possible
 
-		dory.Swim();
-		dory.color = "yellow";
-		dory.Swim();
+		Shark fiery = new Shark();
+		fiery.Eat();
+		fiery.Swim();
+		fiery.SetSharpTeeth(130);
+		fiery.Eat();
+
+		//Fish dory = new Fish(); //another object
+		//dory.Swim();
+		//dory.color = "yellow";
+		//dory.SetColor("yellow"); //change here
+		//dory.Swim();
+
+		//Aquirium fancyAquirium = new Aquirium();
+		//fancyAquirium.fillFish();
+
+
 
 	}
 }
--- Fish.java
+++ Fish.java
@@ -2,10 +2,21 @@
 public class Fish {
 
 	//Atttribute or data member or fields
-	public String color = "blue"; //default
+	private String color = "blue"; //default valueS
 
 	//Method
 	public void Swim () {
 		System.out.println(color + " fish swim");
+	}
+
+	//Setter / Mutator method
+	public void SetColor(String warna) {
+		color = warna;
+
+	}
+
+	//Getter / Accessor method
+	public String GetColor() {
+		return color;
 	}
 }
